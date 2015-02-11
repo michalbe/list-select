9ac7141a3495ce31a0b13283c6c4802bd38a7d46
Michał Budzyński <michal@virtualdesign.pl>
2015-02-11 13:43:55 +0300

    cange input value on click

--- list-select.plugin.js
+++ list-select.plugin.js
@@ -1,24 +1,32 @@
 (function($){
-  $.fn.list_select = function(){
-    return this.each(function(){
-      var input = $(this);
-      input.bind('focus', function() {
-        $('.list-select').animate({
-          height: 60,
-          padding: 10
-        }, 700);
-      });
+  var showList = function() {
+    $('.list-select').animate({
+      height: 60,
+      padding: 10
+    }, 700);
+  };
 
-      input.bind('blur', function(){
-        $('.list-select').animate({
-          height: 0,
-          padding: 0
-        }, 700);
-      })
+  var hideList = function(){
+    $('.list-select').animate({
+      height: 0,
+      padding: 0
+    }, 700);
+  };
+
+  $.fn.list_select = function(){
+    var that = this;
+
+    $('.list-select a').bind('click', function(){
+      that.val($(this).text());
+    });
+
+    return that.each(function(){
+      var input = $(this);
+      input.bind('focus', showList);
+      input.bind('blur', hideList);
     });
   };
 
-  
 })(jQuery);
 
 

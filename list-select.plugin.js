e332eb6fc15c632e2beb98f05f8e15322c1e59b7
Michał Budzyński <michal@virtualdesign.pl>
2015-02-11 14:09:07 +0300

    plugin works only for one input

--- list-select.plugin.js
+++ list-select.plugin.js
@@ -1,29 +1,34 @@
 (function($){
+  var showTime = 300;
+
   var showList = function() {
     $('.list-select').animate({
       height: 60,
       padding: 10
-    }, 700);
+    }, showTime);
   };
 
   var hideList = function(){
     $('.list-select').animate({
       height: 0,
       padding: 0
-    }, 700);
+    }, showTime);
   };
 
   $.fn.list_select = function(){
-    var that = this;
-
-    $('.list-select a').bind('click', function(){
-      that.val($(this).text());
+    var input = this;
+    input.bind('focus', showList);
+    input.bind('blur', hideList);
+    input.bind('click', function(){
+      $(this).select();
     });
 
-    return that.each(function(){
-      var input = $(this);
-      input.bind('focus', showList);
-      input.bind('blur', hideList);
+    $('.list-select a').bind('click', function(){
+      input.val($(this).text());
+    });
+
+    input.bind('keypress', function(){
+
     });
   };
 
